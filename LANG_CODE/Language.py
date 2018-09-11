4b3fc28afa54a5f510f37da62334638637d666a1
Komal <bachhuka.komal@gmail.com>
2018-09-11 10:54:43 -0700

    langauge

---
+++ LANG_CODE/Language.py
@@ -0,0 +1,10 @@
+def greetings(language):
+    if language=='en':
+        print('Hello')
+    elif language=='es':
+        print('Hola')
+    elif language=='fr':
+        print('Bonjour')
+    elif language=='hindi':
+        print('Namaste')
+greetings('es')
